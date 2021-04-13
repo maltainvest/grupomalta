cb78b6879e8e08b0d56350fa3a796366165f7dec
usuariorandom1 <72815503+usuariorandom11@users.noreply.github.com>
2021-04-13 15:29:30 -0500

    1

--- jsa/config.js
+++ jsa/config.js
@@ -1,5 +1,5 @@
 var metacoinConfig = {
-  "contractAddress": "TPoC9aF4TbDM5NGUu3VB5Vk7EE3CcCrGsD",
+  "contractAddress": "TBUuUiMDSbywCPzVYwYUEx6b7a85WmqZDb",
   "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
-  "fullHost": "https://api.nileex.io"
+  "fullHost": "https://api.trongrid.io"
 }
