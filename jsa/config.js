9c3cdd6167dd05220c552681a5d8f908ec5b4670
usuariorandom1 <72815503+usuariorandom11@users.noreply.github.com>
2021-04-21 20:27:49 -0500

    Update config.js

--- jsa/config.js
+++ jsa/config.js
@@ -1,5 +1,5 @@
 var metacoinConfig = {
-  "contractAddress": "TEf3CDrbXcm5tpYscJFftKiATjq3eixF5f",
+  "contractAddress": "TBUuUiMDSbywCPzVYwYUEx6b7a85WmqZDb",
   "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
   "fullHost": "https://api.nileex.io"
 }
